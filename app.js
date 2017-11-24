b6b752074d9e6f3cfb552e2cfb9b9ab3ff475f7a
tnyhfr@gmail.com <Tony@Tonys-MacBook-Air.local>
2017-11-24 15:20:53 -0800

    Initial and final Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+angular
+
+  .module('counter', [])
+  .controller('CounterController', CounterController);
+
+function CounterController() {
+  this.number = 0;
+
+  this.increment = () => { this.number++ };
+  this.decrement = () => { this.number-- };
+}
